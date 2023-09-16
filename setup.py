2295ea9453e6b96f8b779a005f872ecead5849a1
stevieflyer <steveflyer7@gmail.com>
2023-09-16 17:57:48 +0800

    fix bug in twine upload

--- setup.py
+++ setup.py
@@ -9,13 +9,17 @@ def read_requirements():
 
 setup(
     name='tootle',
-    version='0.0.1',
+    version='0.0.1.1',
+    include_package_data=True,
     packages=find_packages(exclude=['test', 'test.*']),
+    package_data={
+        'tootle.dev_bot': ['.autom/*.json'],
+    },
     install_requires=read_requirements(),
     author='',
     author_email='',
     description='',
-    long_description=open('./README.md').read(),
+    long_description=open('./README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
 )
 
